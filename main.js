3289079826ba9fcb3dede3a6c58c30323ac3d343
amrugg <aaron@bibleadventure.com>
2025-06-09 13:50:26 -0500

    Play music

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 (function() {
     "use strict";
-    
     // Note names for display
     var noteNames = ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"];
     
@@ -10,6 +9,7 @@
         var noteIndex = noteNumber % 12;
         return noteNames[noteIndex] + octave;
     }
+    
     
     function onMIDIMessage(event) {
         var data = event.data;
@@ -23,12 +23,55 @@
             var noteInfo = getNoteInfo(noteNumber);
             var message = "Key pressed: " + noteInfo + " (MIDI note " + noteNumber + "), velocity: " + velocity;
             console.log(message);
+            playPianoNote(noteNumber, velocity);
         }
         // Note off (or note on with velocity 0)
         else if (command === 8 || (command === 9 && velocity === 0)) {
             var noteInfo = getNoteInfo(noteNumber);
             var message = "Key released: " + noteInfo + " (MIDI note " + noteNumber + ")";
             console.log(message);
+            releasePianoNote(noteNumber);
+        }
+    }
+    var activeNotes = {};
+    var AudioContext = window.AudioContext || window.webkitAudioContext;
+    var audioCtx = new AudioContext();
+    var pianoPlayer = null;
+    Soundfont.instrument(audioCtx, "acoustic_grand_piano")
+    .then(function(player){
+        pianoPlayer = player;
+        console.log("Piano loaded! 🎹");
+    })
+    .catch(function(err){
+        console.error("Failed to load piano:", err);
+    });
+    function playPianoNote(midiNumber, velocity) {
+        if (!pianoPlayer) {
+            console.warn("Piano not yet loaded.");
+            return;
+        }
+        var noteName = getNoteInfo(midiNumber);
+        // velocity [0–127] → gain [0–1]
+        var gain = (velocity / 127) * 0.8 + 0.2; 
+        // play for 1 second by default
+        var src = pianoPlayer.play(noteName, audioCtx.currentTime, {
+            gain: gain,
+            duration: 10  // plenty of time; we"ll cut it off
+        });
+        console.log("Playing", noteName, "vel", velocity);
+        activeNotes[midiNumber] = src;
+    }
+    function releasePianoNote(midiNumber) {
+        var src = activeNotes[midiNumber];
+        if (src) {
+            try {
+                // Stop almost immediately (you can add a small fade if you like)
+                src.stop(audioCtx.currentTime + 0.02);
+            }
+            catch(e) {
+                console.warn("Could not stop note", midiNumber, e);
+            }
+            delete activeNotes[midiNumber];
         }
     }
     
